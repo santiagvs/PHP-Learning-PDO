805eac86e8deca69972513dc24aefab5a2b953fc
Santiago <victor.lacsilva0102@gmail.com>
2023-01-21 18:47:20 -0300

    feat: adding Phone model to Student

--- src/Domain/Model/Student.php
+++ src/Domain/Model/Student.php
@@ -7,6 +7,10 @@ class Student
     private ?int $id;
     private string $name;
     private \DateTimeInterface $birthDate;
+    /**
+     * @var Phone[]
+     */
+    private array $phones = [];
 
     public function __construct(?int $id, string $name, \DateTimeInterface $birthDate)
     {
@@ -49,5 +53,18 @@ class Student
         return $this->birthDate
             ->diff(new \DateTimeImmutable())
             ->y;
+    }
+
+    public function addPhone(Phone $phone): void
+    {
+        $this->phones[] = $phone;
+    }
+
+    /**
+     * @return Phone[]
+     */
+    public function phones(): array
+    {
+        return $this->phones;
     }
 }
